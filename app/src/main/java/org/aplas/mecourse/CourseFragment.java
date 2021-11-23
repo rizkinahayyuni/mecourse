7ef033c937a924152041cbe257b8434d01ac6433
rizkinahayyuni <rizkinahayyuni@gmail.com>
2021-11-24 02:45:35 +0700

    Add Buttom Navigation

---
+++ app/src/main/java/org/aplas/mecourse/CourseFragment.java
@@ -0,0 +1,28 @@
+package org.aplas.mecourse;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CourseFragment extends Fragment {
+
+    public CourseFragment() {
+        // Required empty public constructor
+    }
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+}
--- app/src/main/java/org/aplas/mecourse/HomeActivity.java
+++ app/src/main/java/org/aplas/mecourse/HomeActivity.java
@@ -1,14 +1,49 @@
 package org.aplas.mecourse;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity{
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.buttom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.course:
+                    fragment = new CourseFragment();
+                    break;
+                case R.id.account:
+                    fragment = new AccountFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container,fragment).commit();
+            return true;
+        }
+    };
 }
